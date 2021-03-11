88e0d765c7875638bbd22eedb0afcd09630ad3f8
Henok <henatan99@gmail.com>
2021-03-11 14:35:46 +0300

    -adds assets folder for css and other asset files and views folders for dom functions files -installs css-loader and style-loader and includes them in modules:rules:use in the webpack.config.js file

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,24 @@
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 var __webpack_exports__ = {};
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '../assets/style.css'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
 
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,27 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+
+  devServer: {
+    contentBase: './dist',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+
 };
