ce955337434e8fc9f30a544bfb1bccb336d2ffd8
Henok <henatan99@gmail.com>
2021-03-12 13:43:33 +0300

    - adds todo.js file which holds a Todo class with title, description, dueDate and priority constructor keys

---
+++ src/classes/todo.js
@@ -0,0 +1,8 @@
+class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
