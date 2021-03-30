b0ce0df2d66e162bc393d560c53d86dcfe080d5d
Henok <henatan99@gmail.com>
2021-03-30 13:47:59 +0300

    adds calendar.test.js file in test/classes folder and addis test: jest in the scripts of package.json

--- test/classes/calendar.test.js
+++ test/classes/calendar.test.js
@@ -1,7 +1,7 @@
 const calendar = require('../../src/classes/calendar');
 
-test('', () => {
-    expect().toBe();
+const calend = new Calendar(2021, 'March')
+
+test('a calendar should return the first week day of the year', () => {
+    expect(calend.dayOne()).toBe(5);
 })
-
-
