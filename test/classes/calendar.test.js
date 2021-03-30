7c4f2fd8fd901afa8d5d0a9e8e12156745b2d610
Henok <henatan99@gmail.com>
2021-03-30 17:57:37 +0300

    adds 3 test cases in calendar.test.js

--- test/classes/calendar.test.js
+++ test/classes/calendar.test.js
@@ -2,6 +2,15 @@ import Calendar from '../../src/classes/calendar';
 
 const calend = new Calendar(2021, 'March');
 
-test('a calendar should return the first week day of the year', () => {
-    expect(calend.dayOne()).toBe(5);
+test('a calendar dayone() property should return the first weekday of the year', () => {
+    expect(calend.dayone()).toBe(5);
 })
+
+test('a calendar start() property should return the first weekday of the month in the year', () => {
+    expect(calend.start()).toBe(1);
+})
+
+test('a calendar monthDays() pproperty should return the first weekday of the month in the year', () => {
+    expect(calend.monthDays()).toBe(31);
+})
+
