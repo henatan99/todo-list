338dd011b15f8e2aa5140b391369c87ce1580c75
Henok <henatan99@gmail.com>
2021-03-31 15:53:37 +0300

    implements the calendar test cases in test/views1/calendar.test.js file using jest spyOn mocking function

--- test/views/calendar.test.js
+++ test/views/calendar.test.js
@@ -1,10 +1,50 @@
-import calendarDIv from '../../src/views1/calendar'
+import calendarDiv from '../../src/views1/calendar'
 
+function isElement(element) {
+    return element instanceof Element || element instanceof HTMLDocument;  
+}
 
-test('should generate and return a project item element', () => {
-    expect(calendarDIv).toBe(true);
+let spy;
+beforeAll(() => {
+  spy = jest.spyOn(document, 'getElementById');
 });
 
-test('should generate and return a project item element', () => {
-    expect(calendarDIv.children[0].classList.contains('yearDiv')).toBe(true);
+describe('with found element', () => {
+    let mockElement;
+    let calendar = calendarDiv();
+
+    beforeEach(() => {
+      mockElement = document.createElement('div');
+      spy.mockReturnValue(mockElement, calendar);
+    });
+
+    it('should have defined mockElement', () => {
+      expect(mockElement).toBeDefined();
+    });
+
+    it('should have defined calendarDiv', () => {
+        expect(calendar).toBeDefined();
+    });
+    
+    it('mockElement should be an element', () => {
+        expect(isElement(mockElement)).toBe(true);
+    });
+
+    it('should have defined calendarDiv element', () => {
+        // mockElement.innerHTML = calendarDIv;
+        expect(isElement(calendar)).toBe(true);
+    });
+
+    // it('should have defined calendarDiv element', () => {
+    //     expect(calendarDIv).toBeDefined();
+    // });
+
+    // it('should have defined calendarDiv element', () => {
+    //     let yeardiv = document.querySelector('#yearDiv');
+    //     expect(calendarDIv.children[0]).toBeDefined();
+    // });
+
+    // test('should generate and return a yearDiv element', () => {
+    //     expect(calendarDIv.children[0].classList.contains('yearDiv')).toBe(true);
+    // });
 });
